87e08942573d9668db06f9d088673ab7f0e1652d
carmeljane <carmelbarwick@gmail.com>
2024-04-01 13:43:01 +0100

    header states and context complete for now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
+import { InputContext } from "../App"
 
 const Header = () => {
     const[value, setValue]= useState("")
@@ -6,7 +7,18 @@ const Header = () => {
 
     const handleInputChange = (e) => {setValue(e.target.value)}
 
-    const handleSubmit = () => {setValue("")}
+    const handleSubmit = () => {
+        setInputValue(value)
+        setValue("")}
+
+    const handleInputKeyDown = (e) => {
+        if (e.key === "Enter") {
+            setInputValue(value)
+            setValue("")
+        }
+    }
+
+
     return (
         <div className="bg-gray-700">
             <div className="container mx-auto py-8">
@@ -14,11 +26,11 @@ const Header = () => {
                 <p className="text-center mt-1 mb-10 text-slate-300 text-lg">Find Maltese Words</p>
                 <div className="flex item-center justify-center mt-5">
                     <div className="flex border-2 border-gray-200 rounded">
-<input className="px-4 py-2 md:w-80" type="text" placeholder="Search for a word" onChange={handleInputChange}/>
+<input className="px-4 py-2 md:w-80" type="text" placeholder="Search for a word" onChange={handleInputChange} value={value} onKeyDown={handleInputKeyDown}/>
 <button className="bg-blue-400 border-l px-4 py-2" onClick={handleSubmit}>Search</button>
                     </div>
                 </div>
-                <h3 className="text-gray-50 text-center mt-4">Result for: <span className="text-white font-bold"></span></h3>
+                {inputValue && <h3 className="text-gray-50 text-center mt-4">Result for: <span className="text-white font-bold">{inputValue}</span></h3>}
         </div>
         </div>
        
