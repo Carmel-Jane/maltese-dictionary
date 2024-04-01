53400f57ffaa8069ddd41f0ec67e3534928ce4c8
carmeljane <carmelbarwick@gmail.com>
2024-04-01 13:01:51 +0100

    use state added to header. Search onCHange, button onClick

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react"
+
 const Header = () => {
+    const[value, setValue]= useState("")
+    const handleInputChange = (e) => {setValue(e.target.value)}
+    const handleSubmit = () => {setValue("")}
     return (
         <div className="bg-gray-700">
             <div className="container mx-auto py-8">
@@ -6,10 +11,11 @@ const Header = () => {
                 <p className="text-center mt-1 mb-10 text-slate-300 text-lg">Find Maltese Words</p>
                 <div className="flex item-center justify-center mt-5">
                     <div className="flex border-2 border-gray-200 rounded">
-<input className="px-4 py-2 md:w-80" type="text" placeholder="Search for a word"/>
-<button className="bg-blue-400 border-l px-4 py-2">Search</button>
+<input className="px-4 py-2 md:w-80" type="text" placeholder="Search for a word" onChange={handleInputChange}/>
+<button className="bg-blue-400 border-l px-4 py-2" onClick={handleSubmit}>Search</button>
                     </div>
                 </div>
+                <h3 className="text-gray-50 text-center mt-4">Result for: <span className="text-white font-bold"></span></h3>
         </div>
         </div>
        
