c5327ae9ccf4294d5e19c70590154eb98f5071b8
carmeljane <carmelbarwick@gmail.com>
2024-04-01 13:51:24 +0100

    result headers created

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Results =() =>{
-
+return(
+    <div className="container mx-auto p-4 max-w-2xl">
+<div>
+<h3 className="text-2xl font-bold mt-4">Definitions</h3>
+<h3 className="text-2xl font-bold mt-4">Root and root type</h3>
+    </div>
+    </div>
+)
 }
 
 export default Results;
