98c9d1e656bdf4122e7917ef9e1102d353646a9f
carmeljane <carmelbarwick@gmail.com>
2024-04-28 14:01:33 +0100

    added base url forapi

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,25 +1,30 @@
 import axios from 'axios';
 
+const api = axios.create({
+  baseURL: process.env.NODE_ENV === 'development' ? '' : 'https://mlrs.research.um.edu.mt/resources/gabra-api',
+});
+
 export const fetchResults = async (inputValue) => {
   try {
-    const res = await axios.get(`/lexemes/search_gloss?s=${inputValue}`);
+    const res = await api.get(`/lexemes/search_gloss?s=${inputValue}`);
     return res.data;
   } catch (error) {
     throw error;
   }
 };
+
 export const fetchWordDetails = async (id) => {
   try {
-    const res = await axios.get(`/lexemes/${id}`);
+    const res = await api.get(`/lexemes/${id}`);
     return res.data;
   } catch (error) {
     throw error;
   }
-}
+};
 
 export const fetchRelatedLexemes = async (id) => {
   try {
-    const res = await axios.get(`/lexemes/related/${id}`);
+    const res = await api.get(`/lexemes/related/${id}`);
     return res.data;
   } catch (error) {
     throw error;
